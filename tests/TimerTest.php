966eb5b124bebb7a1519ad39cdffacbf339411e2
ILYA ERSHOV <ershov.ilya@gmail.com>
2015-12-08 16:59:01 +0300

    PHPUnit timer tests

--- tests/TimerTest.php
+++ tests/TimerTest.php
@@ -8,11 +8,43 @@ class TimerTest extends PHPUnit_Framework_TestCase
 
         // Process
 		$timer->start();
-		usleep(100000);
+		usleep(50000);
 		$timer->stop();
 		$value=$timer();
 		
         // Assert
-        $this->assertEquals(true, ($value<0.15 && $value>0.05));
+        $this->assertEquals(true, ($value>0.045 && $value<0.55));
     }
+	
+    public function testStartParents()
+    {
+		// Init
+        $timer=new Timer();
+
+        // Process
+		$timer->start('mysql.sql.query.response.parsing',true);
+		usleep(50000);
+		$value=$timer('mysql');
+		
+        // Assert
+        $this->assertEquals(true, ($value>0.045 && $value<0.55));
+    }
+	
+    public function testStopChildren()
+    {
+		// Init
+        $timer=new Timer();
+
+        // Process
+		$timer->start('mysql.sql.query.response.parsing',true);
+		usleep(50000);
+		$timer->stop('mysql.sql');
+		usleep(50000);
+		$value=$timer('mysql.sql.query.response.parsing');
+		
+        // Assert
+        $this->assertEquals(true, ($value>0.045 && $value<0.55));
+    }
+	
+	
 }
